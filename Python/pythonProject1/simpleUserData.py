61d7db53f2f334cf3a98c5a1523f38ad8ee92864
John Doe <johndoe@example>
2024-02-12 14:07:21 +0100

    worked out authorization and access to apps

---
+++ Python/pythonProject1/simpleUserData.py
@@ -0,0 +1,20 @@
+user_data = {
+    "users": [
+        {
+            "id": 1,
+            "profile": {
+                "age": 20,
+                "name": "Ethan",
+                "email": "example@email.com"
+            }
+        },
+        {
+            "id": 2,
+            "profile": {
+                "age": 25,
+                "name": "Alice",
+                "email": "alice@example.com"
+            }
+        }
+    ]
+}
